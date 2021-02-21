722c83251cec07a7205a2a0ae375d958015fef5d
Alexander Yonchev <24881674+ayonchev@users.noreply.github.com>
2021-02-21 22:23:09 +0200

    Made the register component usable and fixed some styles.

--- FrontEnd/src/app/components/auth/register/register.component.ts
+++ FrontEnd/src/app/components/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RegisterModel } from 'src/app/models/auth/register.model';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'register',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  model: RegisterModel = {
+    email: '',
+    password: '',
+    confirmPassword: ''
+  };
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  register() {
+    this.authService
+      .register(this.model)
+      .subscribe(res => {
+        this.authService.saveToken(res.token);
+        this.router.navigate(['']);
+      })
+  }
 }
